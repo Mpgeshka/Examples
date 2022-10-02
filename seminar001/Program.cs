f8dda67a8db6f6f8d55af73847cebdd51821e848
Mpgeshka <mpgeshka-05@mail.ru>
2022-10-02 10:57:38 +0500

    add new seminar1

---
+++ seminar001/Program.cs
@@ -0,0 +1,6 @@
+﻿System.Console.Write("Введите число >");
+string inputString = Console.ReadLine();
+int value = Convert.ToInt32(inputString);
+
+int square = value * value;
+System.Console.WriteLine($"Квадрат числа {value} равен {square}");
